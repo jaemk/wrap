fe2ecea2143660743736b8f669d44c6a244d05e3
James Kominick <james.kominick@gmail.com>
2017-04-06 19:10:41 -0400

    update macros and examples

--- examples/basic.rs
+++ examples/basic.rs
@@ -5,6 +5,7 @@ use std::sync::{Mutex, MutexGuard};
 use std::collections::HashMap;
 
 
+/// Setup a fake database
 lazy_static! {
     static ref DB: Mutex<HashMap<String, String>> = {
         let mut m = HashMap::new();
@@ -13,42 +14,101 @@ lazy_static! {
     };
 }
 
-
+/// Setup a fake database connection thing
 #[derive(Debug)]
 struct Conn<'a>(MutexGuard<'a, HashMap<String, String>>);
 impl<'a> Conn<'a> {
     fn query(&self, key: &str) -> Option<String> {
         self.0.get(key).map(|s| s.clone())
     }
+    fn insert(&mut self, key: String, value: String) {
+        self.0.insert(key, value);
+    }
 }
 
 
+/// Define a wrapper that provides a `Conn` instance to wrapped functions
 def_wrapper!{with_conn =
-    before = fn_args >> {
-        println!("log before -> pre_args: {:?}", fn_args);
+    before = (_fn_args) >> {
+        println!("* in `with_conn` wrapper");
+        // you can return early from `before`. this will return before
+        // the wrapped function is executed.
         //return "hijacked!".to_string();
+
         Conn(DB.lock().unwrap())
     };
-    after  = wrapped_result >> {
-        println!("log after -> wrapped_result {:?}", wrapped_result);
+    after  = (wrapped_result) >> {};
+}
+
+
+/// Modifies the return result of a wrapped function that returns an Option<String>
+def_wrapper!{spooky_action =
+    before = (_fn_args) >> {};
+    after  = (wrapped_result) >> {
+        println!("* in `spooky_action` wrapper");
         wrapped_result = wrapped_result.map(|mut s| {
             s.push_str(" !! spooky !!");
             s
         });
-        println!("modifying wrapped_result! -> {:?}", wrapped_result);
     };
 }
 
 
+def_wrapper!{log =
+    before = (fn_args) >> {
+        println!("* [log] >> before everything! fn_args: {:?}", fn_args);
+    };
+    after  = (wrapped_result) >> {
+        println!("* [log] >> after everything! wrapped_result: {:?}", wrapped_result);
+    };
+}
+
+
+/// Generate a function wrapped with `with_conn` to provide a binding
+/// `conn` of type `Conn` to the function body
 wrap_with!{with_conn -> conn: Conn; >>
 fn get_key(key: &str) -> Option<String> = {
-    println!(" in wrapped! ->> {:?}", conn);
     let v: Option<String> = conn.query(key);
-    println!("found: {:?}", v);
     v
 }}
 
+
+/// Generate a function wrapped with `spooky_action` that modifies
+/// this function body's return value
+wrap_with!{spooky_action -> _nil: (); >>
+fn get_key_spooky(key: &str) -> Option<String> = {
+    get_key(key)
+}}
+
+
+/// Wrap get-spooky with a logging wrapper. Note, that
+/// the wrapper ident and return type can be excluded when the
+/// wrapper's `before` expression provides `()`
+wrap_with!{log >>
+fn get_key_spooky_logged(key: &str) -> Option<String> = {
+    get_key_spooky(key)
+}}
+
+
+wrap_with!{with_conn -> mut conn: Conn; >>
+fn set_key(key: String, value: String) -> () = {
+    conn.insert(key, value);
+}}
+
+
 pub fn main() {
+    println!("setting key...");
+    set_key("james".into(), "JAMES".into());
+
+    println!("\ngetting key...");
     let v: Option<String> = get_key("a");
-    println!("{:?}", v);
+    println!("get_key(\"a\") -> {:?}", v);
+
+    println!("\ngetting key [spooky]...");
+    let v = get_key_spooky("a");
+    println!("get_key_spooky(\"a\") -> {:?}", v);
+
+    println!("\ngetting key [spooky & logged]...");
+    let v = get_key_spooky_logged("a");
+    println!("get_key_spooky_logged(\"a\") -> {:?}", v);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,42 @@
 
 #[macro_export]
 macro_rules! def_wrapper {
-    ($name:ident = before = $fn_args:ident >> $before:expr ; after = $wrapped_res:ident >> $after:expr;) => {
+    ($name:ident = before = ($fn_args:ident) >> $before:expr ; after = ($wrapped_res:ident) >> $after:expr;) => {
         macro_rules! $name {
-            ($args:expr; $before_block_res:ident : $before_block_ty:ty ; $wrapped_body:expr => $wrapped_body_ret:ty) => {
+            ($before_block_res:ident : $before_block_ty:ty ; $args:expr => $wrapped_body_ret:ty = $wrapped_body:expr) => {
                 {
-                    // assign wrapped function's args (collected in a tuple)
-                    // to our `fn_args` ident, to make available to `before` and `after` blocks.
-                    let $fn_args = $args;
 
-                    // eval `before` block and assign to the `before_block_res` ident supplied by `wrap_with!`
-                    let $before_block_res: $before_block_ty = $before;
+                    let mut $before_block_res: $before_block_ty = {
+                        // assign wrapped function's args (refs to args collected in a tuple)
+                        // to our `fn_args` ident, to make available to the `before` block
+                        let $fn_args = $args;
 
-                    // eval our wrapped function, providing `before_block_res` to the `wrapped_body`,
-                    // assigning the result to our ``wrapped_res` ident to make available to our `after` block.
-                    let mut $wrapped_res: $wrapped_body_ret = (|$before_block_res: $before_block_ty| $wrapped_body)($before_block_res);
+                        // eval the `before` block and assign to the `before_block_res` ident supplied by `wrap_with!`
+                        $before
+                    };
 
-                    // eval `after` block. Can mutate the `wrapped_res` if necessary
+                    // eval our wrapped function, providing `&mut before_block_res` to the `wrapped_body`,
+                    // assigning the result to our ``wrapped_res` ident to make available to our `after` block and
+                    // also keep `before_block_res` in scope for the `after` block
+                    let mut $wrapped_res: $wrapped_body_ret = (|$before_block_res: &mut $before_block_ty| $wrapped_body)(&mut $before_block_res);
+
+                    // eval the `after` block. Can access and mutate both `wrapped_res` and `before_block_res` if necessary.
+                    // cannot access `fn_args` because they are likely consumed by the wrapped function.
                     let _ = $after;
 
+                    $wrapped_res
+                }
+            };
+
+            // same as above, but accept and pass along `mut` keyword
+            (mut $before_block_res:ident : $before_block_ty:ty ; $args:expr => $wrapped_body_ret:ty = $wrapped_body:expr) => {
+                {
+                    let mut $before_block_res: $before_block_ty = {
+                        let $fn_args = $args;
+                        $before
+                    };
+                    let mut $wrapped_res: $wrapped_body_ret = (|mut $before_block_res: &mut $before_block_ty| $wrapped_body)(&mut $before_block_res);
+                    let _ = $after;
                     $wrapped_res
                 }
             };
@@ -29,10 +47,25 @@ macro_rules! def_wrapper {
 
 #[macro_export]
 macro_rules! wrap_with {
+    ($wrapper:ident >> fn $name:ident ($($arg:ident : $argtype:ty),*) -> $ret:ty = $body:expr) => {
+        #[allow(unused_mut, unused_parens)]
+        pub fn $name($($arg : $argtype),*) -> $ret {
+            $wrapper!( _before_block_res : () ;  ($(&$arg),*) => $ret = $body)
+        }
+    };
     ($wrapper:ident -> $before_block_res:ident : $before_block_ty:ty; >> fn $name:ident ($($arg:ident : $argtype:ty),*) -> $ret:ty = $body:expr) => {
         #[allow(unused_mut, unused_parens)]
         pub fn $name($($arg : $argtype),*) -> $ret {
-            $wrapper!( ($($arg),*) ; $before_block_res : $before_block_ty ; $body => $ret)
+            $wrapper!( $before_block_res : $before_block_ty ;  ($(&$arg),*) => $ret = $body)
         }
-    }
+    };
+    // pass along `mut ident`
+    ($wrapper:ident -> mut $before_block_res:ident : $before_block_ty:ty; >> fn $name:ident ($($arg:ident : $argtype:ty),*) -> $ret:ty = $body:expr) => {
+        #[allow(unused_mut, unused_parens)]
+        pub fn $name($($arg : $argtype),*) -> $ret {
+            $wrapper!( mut $before_block_res : $before_block_ty ;  ($(&$arg),*) => $ret = $body)
+        }
+    };
 }
+
+
